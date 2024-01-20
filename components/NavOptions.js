76864ef92caafcd2c3201d3d21f5456dd70f3111
Vaibhav Chalapakula <vaibhavch304@gmail.com>
2024-01-21 01:40:44 +0530

    add: Added the Navigation between the screens.

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import tailwind from "twrnc";
 import { Icon } from "@rneui/base";
+import { useNavigation } from "@react-navigation/native";
 
 const data = [
   {
@@ -19,6 +20,7 @@ const data = [
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
   return (
     <FlatList
       keyExtractor={(item) => item.id}
@@ -26,6 +28,7 @@ const NavOptions = () => {
       horizontal
       renderItem={({ item }) => (
         <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
           style={tailwind`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-45 rounded-md`}
         >
           <View>
